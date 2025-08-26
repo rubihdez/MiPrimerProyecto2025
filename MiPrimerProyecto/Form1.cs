1db37e0a8fcacce32867f3c6d601e0b69f3fefc7
Rubihdez <alu.24130045@correo.itlalaguna.edu.mx>
2025-08-26 15:25:22 -0600

    SE AGREGO NUEVA FUNCIONALIDAD PARA DESPECIRSE

--- MiPrimerProyecto/Form1.cs
+++ MiPrimerProyecto/Form1.cs
@@ -21,5 +21,10 @@ namespace MiPrimerProyecto
         {
             MessageBox.Show("HOLA MUNDO");
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("ADIOS MUNDO");
+        }
     }
 }
