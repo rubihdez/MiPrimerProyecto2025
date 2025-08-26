494c3da41b93c6eaca4574de7142bb783a377020
Rubihdez <alu.24130045@correo.itlalaguna.edu.mx>
2025-08-26 15:20:16 -0600

    SE MODIFICA PANTALLA PRINCIPAL

--- MiPrimerProyecto/Form1.Designer.cs
+++ MiPrimerProyecto/Form1.Designer.cs
@@ -28,20 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(395, 245);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(187, 97);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "boton";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(922, 530);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "MiPrimerProyecto2025";
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
 
